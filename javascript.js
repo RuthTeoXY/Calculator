5dcc0c9e10ce52a2f1d159884574aeec12e6d802
Ruth Teo <rooftiop_61@hotmail.com>
2022-07-17 23:22:25 +0800

    design is out. event listener works

--- javascript.js
+++ javascript.js
@@ -13,3 +13,42 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y;
 }
+
+function operate(op, x, y) {
+  return op(x, y);
+}
+
+//add event listener to all buttons
+//return number or operator store in a variable
+//append number into text content of display
+
+const operators = ["+", "-", "x", "/", "%"];
+let runningDisplay = document.querySelector(".runningDisplay");
+let summingDisplay = document.querySelector(".summingDisplay");
+let tempHandler = [];
+
+//Handling Number
+const numButtons = document.querySelectorAll(".num");
+numButtons.forEach((numButton) => numButton.addEventListener("click", display));
+
+function display(e) {
+  let text = e.target.innerText;
+  runningDisplay.textContent += text;
+  tempHandler.push(text);
+}
+
+//Handling Operators
+const opButtons = document.querySelectorAll(".operation");
+opButtons.forEach((opButton) =>
+  opButton.addEventListener("click", displayAndOp)
+);
+
+function displayAndOp(e) {
+  let text = e.target.innerText;
+  runningDisplay.textContent += text;
+  tempHandler.push(text);
+}
+
+//When to calculate
+//- when equal button is clicked
+//- when there is varX, operator, varY, and an operator is called
