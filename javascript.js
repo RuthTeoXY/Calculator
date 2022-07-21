d2556a7b545ef07586b5291d4de8589e9539be27
Ruth Teo <rooftiop_61@hotmail.com>
2022-07-22 00:44:49 +0800

    generic math works, back and clear works too

--- javascript.js
+++ javascript.js
@@ -11,11 +11,28 @@ function multiply(x, y) {
 }
 
 function divide(x, y) {
+  console.log(x);
+  console.log(y);
+  console.log(x / y);
   return x / y;
 }
 
+function percentage(x) {
+  return x / 100;
+}
+
 function operate(op, x, y) {
-  return op(x, y);
+  if (op === "+") {
+    return add(x, y);
+  } else if (op === "-") {
+    return subtract(x, y);
+  } else if (op === "x") {
+    return multiply(x, y);
+  } else if (op === "/") {
+    return divide(x, y);
+  } else if (op === "%") {
+    return percentage(x);
+  }
 }
 
 //add event listener to all buttons
@@ -23,32 +40,110 @@ function operate(op, x, y) {
 //append number into text content of display
 
 const operators = ["+", "-", "x", "/", "%"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let runningDisplay = document.querySelector(".runningDisplay");
 let summingDisplay = document.querySelector(".summingDisplay");
-let tempHandler = [];
+let tempVariableX = "";
+let tempVariableY = "";
+let tempOp = "";
+
+function lastChar(string) {
+  return string.substring(string.length - 1);
+}
+
+function secLastChar(string) {
+  return string.substring(
+    runningDisplay.textContent.length - 2,
+    runningDisplay.textContent.length - 1
+  );
+}
 
 //Handling Number
 const numButtons = document.querySelectorAll(".num");
-numButtons.forEach((numButton) => numButton.addEventListener("click", display));
+numButtons.forEach((numButton) =>
+  numButton.addEventListener("click", displayNum)
+);
 
-function display(e) {
+function displayNum(e) {
   let text = e.target.innerText;
   runningDisplay.textContent += text;
-  tempHandler.push(text);
 }
 
 //Handling Operators
 const opButtons = document.querySelectorAll(".operation");
-opButtons.forEach((opButton) =>
-  opButton.addEventListener("click", displayAndOp)
-);
+opButtons.forEach((opButton) => opButton.addEventListener("click", displayOp));
 
-function displayAndOp(e) {
-  let text = e.target.innerText;
-  runningDisplay.textContent += text;
-  tempHandler.push(text);
+function displayOp(e) {
+  if (runningDisplay.textContent != "") {
+    let text = e.target.innerText;
+    if (tempVariableX === "") {
+      tempVariableX = runningDisplay.textContent;
+    } else {
+      tempVariableY = runningDisplay.textContent;
+    } //last numbers are y
+    run(runningDisplay.textContent); //evaluate
+    summingDisplay.textContent += text; //move y up add op
+    runningDisplay.textContent = ""; //clear bottom
+    tempOp = text;
+    console.log(tempVariableX);
+    console.log(tempVariableY);
+    console.log(tempOp);
+  }
+
+  // if (tempOp === "=") {
+  //   runningDisplay.textContent = result;
+  // }
+
+  // tempVariableX = runningDisplay.textContent; //results become new x
+
+  // if (operators.includes(lastChar(summingDisplay.textContent)) === false) {
+  //   tempVariableX = runningDisplay.textContent; //last numbers are x
+  //   runningDisplay.textContent = ""; //clear bottom
+  //   summingDisplay.textContent = tempVariableX + text; //move x up add op
+  //   tempOp = text; //for evaluation
+  // }
+  // tempVariableY = runningDisplay.textContent;
+  // summingDisplay.textContent += tempVariableY;
+  // runningDisplay.textContent = "";
+  // run(runningDisplay.textContent);
+  // summingDisplay.textContent = runningDisplay.textContent + text;
+  // }
 }
 
 //When to calculate
 //- when equal button is clicked
 //- when there is varX, operator, varY, and an operator is called
+//After calculating, the result goes into tempVariable X
+function run(str) {
+  if (tempVariableY != "") {
+    let result = operate(
+      tempOp,
+      parseFloat(tempVariableX),
+      parseFloat(tempVariableY)
+    );
+    summingDisplay.textContent = result;
+    tempVariableX = result;
+    console.log(result);
+  }
+}
+
+//Handling clear and back
+const clButton = document.querySelector("#cl");
+clButton.addEventListener("click", clearAll);
+const backButton = document.querySelector("#back");
+backButton.addEventListener("click", backspace);
+
+function clearAll() {
+  runningDisplay.textContent = "";
+  summingDisplay.textContent = "";
+  tempVariableX = "";
+  tempVariableY = "";
+  tempOp = "";
+}
+
+function backspace() {
+  runningDisplay.textContent = runningDisplay.textContent.substring(
+    0,
+    runningDisplay.textContent.length - 1
+  );
+}
